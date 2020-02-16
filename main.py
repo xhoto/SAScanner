95dea667ea7659b97b9fcb27c81cd3409994dac8
xhoto <xhoto.choi@gmail.com>
2020-02-16 23:20:28 +0900

    Add RangeSearch

--- main.py
+++ main.py
@@ -1,123 +1,171 @@
+import os
+import sys
 import time
 from datetime import datetime
+from datetime import date
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import sa_config as sc
 
-# download chromedriver https://sites.google.com/a/chromium.org/chromedriver/home
-driver = webdriver.Chrome() 
-wait = WebDriverWait(driver, 10)
+def SelectDate(dYear, dMonth, dDay):
+    # 날짜선택
+    btnCal = driver.find_element_by_id("sCalendar1")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnCal).click().perform()
 
-# open main url
-driver.get(sc.main_url)
-time.sleep(2)
+    moveStep = 0
+    monthOfToday = date.today().month
 
-# find login url in main page
-# "action_logging_common('TOP_03'); cms.goToLink('/I/KR/KO/viewLogin.do?callType=IBE&menuId=CM201802220000728453');"
-login_url = driver.find_element_by_link_text('로그인').get_attribute('onclick')
-login_url = login_url.replace("action_logging_common('TOP_03'); cms.goToLink('","")
-login_url = sc.main_url + login_url.replace("');","")
+    if monthOfToday > dMonth: # 2 1 > 10번 # 4 2 > 8번
+        moveStep = 9 + (monthOfToday - dMonth)
+    elif monthOfToday == dMonth or monthOfToday == dMonth + 1:
+        moveStep = 0
+    else:
+        moveStep = dMonth - (monthOfToday+1) 
 
-# open login page
-driver.get(login_url)
-time.sleep(5)
+    for i in range(0, moveStep):
+        btnNextMonth = driver.find_element_by_xpath("//a[@data-handler='next'][@title='다음달']")
+        mouse = webdriver.ActionChains(driver)
+        mouse.move_to_element(btnNextMonth).click().perform()
+        time.sleep(0.5)
 
-userid = driver.find_element_by_id('txtID')
-userid.send_keys(sc.login_ID)
-time.sleep(1)
+    btnDay4 = driver.find_element_by_xpath("//td[@data-month='" + str(dMonth - 1) + "'][@data-year='" + str(dYear) + "']//a[@class='ui-state-default'][text()='" + str(dDay) + "']")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnDay4).click().perform()
 
-password = driver.find_element_by_id('txtPW')
-password.send_keys(sc.login_PW)
-time.sleep(1)
+def SelectAirport(xpathAirportSearch, airportCode):
 
+    btnAirportSearch = driver.find_element_by_xpath(xpathAirportSearch)
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnAirportSearch).click().perform()
+    time.sleep(0.5)
 
-loginbtn = driver.find_element_by_id('btnLogin')
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(loginbtn).click().perform()
-time.sleep(5)
+    txtAirport = driver.find_element_by_xpath("//div[@class='flights_list star_air']/input")
+    txtAirport.send_keys(airportCode)
 
-# open star alliance search page
-driver.get(sc.search_url)
-time.sleep(5)
+    btnAirportSearch2 = driver.find_element_by_xpath("//div[@class='flights_list star_air']/button")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnAirportSearch2).click().perform()
+    time.sleep(0.5)
 
-# 출발공항 선택
-btnDepartureAirport = driver.find_element_by_xpath("//div[@class='itinerary_select spot_proven']/a")
-departureAirportAction = webdriver.ActionChains(driver)
-departureAirportAction.move_to_element(btnDepartureAirport).click().perform()
-time.sleep(5)
+    btnSelectedAirport = driver.find_element_by_xpath("//li[@airport='" + airportCode + "']/a")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnSelectedAirport).click().perform()
+    time.sleep(0.5)
 
-txtAirport = driver.find_element_by_xpath("//div[@class='flights_list star_air']/input")
-txtAirport.send_keys('ICN')
+def Search(dAirportCode, aAirportCode, dYear, dMonth, dDay, classE=True, classB=True, adultCount=3):
+    
+    # open star alliance search page
+    driver.get(sc.search_url)
+    time.sleep(2)
 
-bbb = driver.find_element_by_xpath("//div[@class='flights_list star_air']/button")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(bbb).click().perform()
-time.sleep(1)
+    # 출발공항
+    SelectAirport("//div[@class='itinerary_select spot_proven']/a", dAirportCode)
+    # 도착공항
+    SelectAirport("//div[@class='itinerary_select spot_destin']/a", aAirportCode)
+    # 날짜
+    SelectDate(dYear, dMonth, dDay)
 
-btnSelectedAirport = driver.find_element_by_xpath("//li[@airport='ICN']/a")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnSelectedAirport).click().perform()
-time.sleep(1)
+    # 탑승인원 (성인만 선택, 소아/유아는 전화로 예약해야됨)
+    txtAdultCount = driver.find_element_by_id("adultCount")
+    txtAdultCount.send_keys(adultCount)
 
-# 도착공항 선택
-btnArrivalAirport = driver.find_element_by_xpath("//div[@class='itinerary_select spot_destin']/a")
-arrivalAirportAction = webdriver.ActionChains(driver)
-arrivalAirportAction.move_to_element(btnArrivalAirport).click().perform()
-time.sleep(5)
+    # 좌석등급
+    if classE:
+        btnClassE = driver.find_element_by_xpath("//a[@cabinclass='E']")
+        mouse = webdriver.ActionChains(driver)
+        mouse.move_to_element(btnClassE).click().perform()
 
-txtAirport = driver.find_element_by_xpath("//div[@class='flights_list star_air']/input")
-txtAirport.send_keys('FRA')
+    if classB:
+        btnClassB = driver.find_element_by_xpath("//a[@cabinclass='B']")
+        mouse = webdriver.ActionChains(driver)
+        mouse.move_to_element(btnClassB).click().perform()
 
-bbb = driver.find_element_by_xpath("//div[@class='flights_list star_air']/button")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(bbb).click().perform()
-time.sleep(1)
+    # 검색
+    btnCL = driver.find_element_by_id("btn_coupon_layer")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnCL).click().perform()
+    time.sleep(2)
 
-btnSelectedAirport = driver.find_element_by_xpath("//li[@airport='FRA']/a")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnSelectedAirport).click().perform()
-time.sleep(1)
+    btnSS = driver.find_element_by_xpath("//button[text()='예매 진행']")
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(btnSS).click().perform()
 
-###########################
-# 날짜선택
-# hiddenDepartureDate = driver.find_element_by_xpath("//input[@id='departureDate']")
-# hiddenDepartureDate.setAttribute("value", "20200306")
+    time.sleep(20)
 
-## TODO 날짜 선택
-# btn_coupon_layer
-btnCal = driver.find_element_by_id("sCalendar1")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnCal).click().perform()
-time.sleep(1)
+    dDate = datetime(year=dYear, month=dMonth, day=dDay)
 
-#######################
+    # 검색 결과 출력
+    flights = driver.find_elements_by_xpath("//tr[@class='flight'][@layover='false']")
+    if len(flights) == 0:
+        print(dDate.strftime("%Y-%m-%d") + '\t' + dAirportCode + '\t' + aAirportCode + '\tnone')
+    else:
+        for flight in flights:
+            flight = flight.text \
+                .replace('\n루프트한자 독일 항공 운항', '') \
+                .replace('\n운항', '') \
+                .replace('시간',':') \
+                .replace('분','') \
+                .replace('직항','0') \
+                .replace('석','') \
+                .replace('\n+1Day','+1 ') \
+                .replace('\n','\t')
+            print(dDate.strftime("%Y-%m-%d") + ' ' + (dDate.strftime('%A'))[:3] + '\t' + dAirportCode + '\t' + aAirportCode + '\t' + flight)
+    
+    time.sleep(5)
 
-# 성인
-txtAdultCount = driver.find_element_by_id("adultCount")
-txtAdultCount.send_keys('1')
+def SearchRange(dAirportCode, aAirportCode, startMonth, endMonth, classE=True, classB=True):
+    daysInMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for m in range(startMonth, endMonth+1):
+        for d in range(1, daysInMonth[m]):
+            Search(dAirportCode, aAirportCode, 2020, m, d, classE, classB)
 
-# 소아
-txtChildCount = driver.find_element_by_id("childCount")
-txtChildCount.send_keys('0')
+def main():
+    a1 = sys.argv[1]
+    a2 = sys.argv[2]
 
-# class
-btnClass = driver.find_element_by_xpath("//a[@name='cabinClassAll']")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnClass).click().perform()
+    # download chromedriver https://sites.google.com/a/chromium.org/chromedriver/home
 
-# btn_coupon_layer
-btnCL = driver.find_element_by_id("btn_coupon_layer")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnCL).click().perform()
-time.sleep(1)
+    wait = WebDriverWait(driver, 10)
 
-# btn_M red
-btnSS = driver.find_element_by_xpath("//button[@class='btn_M red']")
-mouse = webdriver.ActionChains(driver)
-mouse.move_to_element(btnSS).click().perform()
+    # 아시아나 접속
+    driver.get(sc.main_url)
+    time.sleep(2)
 
-print ('test')
+    # 로그인페이지 이동
+    login_url = driver.find_element_by_link_text('로그인').get_attribute('onclick')
+    login_url = login_url.replace("action_logging_common('TOP_03'); cms.goToLink('","")
+    login_url = sc.main_url + login_url.replace("');","")
+    driver.get(login_url)
+
+    #로그인
+    userid = driver.find_element_by_id('txtID')
+    userid.send_keys(sc.login_ID)
+    password = driver.find_element_by_id('txtPW')
+    password.send_keys(sc.login_PW)
+    loginbtn = driver.find_element_by_id('btnLogin')
+    mouse = webdriver.ActionChains(driver)
+    mouse.move_to_element(loginbtn).click().perform()
+    time.sleep(2)
+
+    classE = False
+    classB = True
+
+    if classE and classB:
+        print('날짜\t\t출발\t도착\t출발T\t비행\t경유\t도착T\t편명\t기종\tEco\tBiz')
+    elif classE:
+        print('날짜\t\t출발\t도착\t출발T\t비행\t경유\t도착T\t편명\t기종\tEco')
+    elif classB:
+        print('날짜\t\t출발\t도착\t출발T\t비행\t경유\t도착T\t편명\t기종\tBiz')
+
+    SearchRange(a1, a2, 7, 7, classE, classB)
+    SearchRange(a2, a1, 7, 7, classE, classB)
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome() 
+    main()
+
+driver.close()
